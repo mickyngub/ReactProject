4f31346463486d18b52d8e5f2a3cd0d21c5cfc83
narit789 <narit789@Mickys-MacBook-Pro.local>
2020-03-13 22:47:09 +0700

    passing function using props

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -3,12 +3,13 @@ import React from "react";
 type PersonProps = {
   name?: any;
   children?: any;
+  click?: any;
 };
 
 const Person = (props: PersonProps) => {
   return (
     <div>
-      <p>
+      <p onClick={props.click}>
         I'm {props.name} and I'm {Math.random()} years old!
       </p>
       <p>{props.children}</p>
--- src/components/State.tsx
+++ src/components/State.tsx
@@ -14,7 +14,10 @@ class NewPost extends Component {
     return (
       <div>
         <People name={this.state.person[0].name} />
-        <People name={this.state.person[1].name} />
+        <People
+          name={this.state.person[1].name}
+          click={this.switchNameHandler}
+        />
         <button onClick={this.switchNameHandler}>Switch Name</button>
       </div>
     );
